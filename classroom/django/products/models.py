46b281088d9026ea9440d3615f91c18c89d593c2
J1R-A1R <aoboxtvyt@gmail.com>
2026-06-30 21:32:55 +0400

    django

---
+++ classroom/django/products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True, default="Apranq")
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    valid = models.DateField()
+    quantity = models.PositiveIntegerField(default=0)
